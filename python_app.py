49e45f834ac275cb2df5e55ff925de538c1780ae
Rachel Collins <spiritedndiligent@gmail.com>
2023-04-25 23:54:51 -0400

    updating variables in file

--- python_app.py
+++ python_app.py
@@ -3,3 +3,13 @@ from app2 import password
 
 email_sender = 'spiritedndiligent@gmail.com'
 email_password = password
+
+email_receiver = ''
+
+subject = 'Please remember to subscribe'
+body = ''
+
+em = EmailMessage()
+em['From'] = email_sender
+em['To'] = email_receiver
+em['subject']
