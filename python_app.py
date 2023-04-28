29f6f4d6e8ac227bb5e4573272f3bd345726847c
Rachel Collins <spiritedndiligent@gmail.com>
2023-04-27 23:47:03 -0400

    smtp again

--- python_app.py
+++ python_app.py
@@ -19,4 +19,6 @@ em.set_content(body)
 
 context = ssl.create_default_context()
 
-with smtplib.SMTP_SSL('smtp.gmail.com', 465, context=context)
+with smtplib.SMTP_SSL('smtp.gmail.com', 465, context=context) as smtp:
+    smtp.login(email_sender, email_password)
+    smtp.sendmail(email_sender, email_receiver, em.as_string())
