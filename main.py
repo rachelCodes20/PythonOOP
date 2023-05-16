8020678c6707360b8f3660d0613de12ecf95c284
Rachel Collins <spiritedndiligent@gmail.com>
2023-05-15 21:56:11 -0400

    lines function complete

--- main.py
+++ main.py
@@ -17,15 +17,15 @@ def deposit():
 def get_number_of_lines():
      while True:
         lines = input("Enter the number of lines to bet on (1-" + str(MAX_LINES) + ")?")
-        if amount.isdigit():
-            amount = int(amount)
-            if amount > 0:
+        if lines.isdigit():
+            lines = int(lines)
+            if 1 <= lines <= MAX_LINES:
                 break
             else:
-                print("Amount must be greater than 0.")
+                print("Enter a valid number of lines.")
         else:
             print("Please enter a number.")
-    return amount
+        return lines
 
 def main():
     balance = deposit()
