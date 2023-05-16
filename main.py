39c297d831c07d8ab52d7c76d3ba791d6984fe76
Rachel Collins <spiritedndiligent@gmail.com>
2023-05-15 21:22:00 -0400

    git add main.py

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+
+MAX_LINES = 3
+
 def deposit():
     while True:
         amount = input("What would you like to deposit? $")
@@ -12,7 +15,18 @@ def deposit():
     return amount
 
 def get_number_of_lines():
-    
+     while True:
+        lines = input("Enter the number of lines to bet on (1-" + str(MAX_LINES) + ")?")
+        if amount.isdigit():
+            amount = int(amount)
+            if amount > 0:
+                break
+            else:
+                print("Amount must be greater than 0.")
+        else:
+            print("Please enter a number.")
+    return amount
+
 def main():
     balance = deposit()
     
