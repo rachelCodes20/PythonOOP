6102f39f060fce947c850ef48c2817068d392912
Rachel Collins <spiritedndiligent@gmail.com>
2023-05-16 22:52:28 -0400

    starting get bet func

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 
 MAX_LINES = 3
+MAX_BET = 100
+MIN_BET = 1
 
 def deposit():
     while True:
@@ -26,7 +28,19 @@ def get_number_of_lines():
         else:
             print("Please enter a number.")
         return lines
-
+     
+def get_bet():
+    while True:
+        amount = input("What would you like to deposit? $")
+        if amount.isdigit():
+            amount = int(amount)
+            if amount > 0:
+                break
+            else:
+                print("Amount must be greater than 0.")
+        else:
+            print("Please enter a number.")
+    return amount
 def main():
     balance = deposit()
     
